b12574c517af03df6bb4dc185e007ac1760b4abc
Shiwang Solanki <shiwangsolanki27@gmail.com>
2025-09-10 00:10:20 +0530

    CRUD features updated

--- src/main/java/com/app/blog/Payloads/UserDto.java
+++ src/main/java/com/app/blog/Payloads/UserDto.java
@@ -1,5 +1,6 @@
 package com.app.blog.Payloads;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,9 +11,19 @@ import lombok.Setter;
 public class UserDto {
 	
 	private int id ;
+	
+	@jakarta.validation.constraints.NotEmpty
+	@Size(min = 4, message = "username must be min of 4 characters!!!")
 	private String name;
+	
+	@jakarta.validation.constraints.Email(message = "email address is not valid")
 	private String email;
+	
+	@jakarta.validation.constraints.NotEmpty
+	@Size(min =3, max = 10, message = "Password must be of 3 characters and max of 10 characters")
 	private String password;
+	
+	@jakarta.validation.constraints.NotEmpty
 	private String about;	
 	
 
