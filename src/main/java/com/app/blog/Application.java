ad858ce98a3e0ddc9328cfb4363204f20242c943
Shiwang Solanki <shiwangsolanki27@gmail.com>
2025-09-10 19:50:56 +0530

    Category feature added

--- src/main/java/com/app/blog/Application.java
+++ src/main/java/com/app/blog/Application.java
@@ -1,13 +1,24 @@
 package com.app.blog;
 
+import org.modelmapper.ModelMapper; // <-- Add this line
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@Configuration
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.app.blog.repository")
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
--- src/main/java/com/app/blog/Controller/UserController.java
+++ src/main/java/com/app/blog/Controller/UserController.java
@@ -1,9 +1,16 @@
 package com.app.blog.Controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,17 +18,46 @@ import org.springframework.web.bind.annotation.RestController;
 import com.app.blog.Payloads.UserDto;
 import com.app.blog.Service.UserService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 	
-	 @Autowired
-	 private UserService userService;
-	 
-	 @PostMapping("/")
-	 public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
-		 UserDto createUserDto = this.userService.createUser(userDto);
-		 return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
-	 }
-
+	@Autowired
+	private UserService userService;
+	
+	// CREATE User
+	@PostMapping("/")
+	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto){
+		UserDto createUserDto = this.userService.createUser(userDto);
+		return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
+	}
+	
+	// UPDATE User
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto,
+	                                          @PathVariable("userId") Integer userId){
+		UserDto updatedUser = this.userService.updateUser(userDto, userId);
+		return ResponseEntity.ok(updatedUser);
+	}
+	
+	// DELETE User
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<?> deleteUser(@PathVariable("userId") Integer uid) {
+		this.userService.deleteUser(uid);
+		return new ResponseEntity<>(Map.of("message", "User deleted Successfully"), HttpStatus.OK);
+	}
+	
+	// GET all users
+	@GetMapping("/")
+	public ResponseEntity<List<UserDto>> getAllUser(){
+		return ResponseEntity.ok(this.userService.getAllUsers());
+	}
+	
+	// GET single user
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto> getSingleUser(@PathVariable Integer userId){
+		return ResponseEntity.ok(this.userService.getUserById(userId));
+	}
 }
